3cf7c0e0d259680b2103e5d3b4e57c363fd04e09
Derek <derekcerretani@gmail.com>
2013-10-08 11:16:17 -0700

    Update variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+var x = 2013
+var y = 1988
+var z = x - y
+console.log(z)
+
+var question = "25 is a" + " "
+var good = "good age to be."
+var bad = "bad age to be."
+console.log(question + good)
+
+console.log(y + bad)
